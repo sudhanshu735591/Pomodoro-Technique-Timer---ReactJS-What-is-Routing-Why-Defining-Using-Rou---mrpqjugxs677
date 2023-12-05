cf63cb8053a865fee76c0a6ba7ec37af07f81cf1
Sudhanshu <ersshekhar05@gmail.com>
2023-12-05 11:42:18 +0530

    updated code

--- app/page.js
+++ app/page.js
@@ -1,10 +1,129 @@
+
+"use client";
+import { useEffect, useState } from 'react';
+import './globals.css';
+
+
 const App = () => {
 
 
-  return (
-    <div id="main">
+  let [workDuration, setWorkDuration] = useState(25*60);
+
+  let [durationChange, setDurationChange] = useState(0);
+
+  let [start, setStart] = useState(durationChange);
+
+  let [breakChange, setBreakChange] = useState(0);
+
+  let [flag, setFlag] = useState(false);
+
+  let [disabled, setDisable] = useState(false);
+
+  let[stopDisability, setStopDisablity] = useState(true);
+
+  let [resetDisability, setResetDisablity] = useState(true);
+
+  let [count, setCount] = useState(0);
+
+  let [alertText, setAlertText] = useState("work duration is over");
+  let [textTime, setTextTime] = useState("Work-Time")
+
+  function Timer(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  }
+
+
+  function handleCLick(){
+    setWorkDuration(durationChange*60);
+    setFlag(false);
+    setDisable(false);
+    setResetDisablity(false);
+  }
+
+
+
+  useEffect(() => {
+    let timer;
     
+    if(flag){
+      timer = setTimeout(() => {
+
+        if (workDuration < 1) {
+          alert(alertText);
+
+          setCount(count+1);
+
+          console.log(count);
+
+          if(count%2==0){
+            setWorkDuration(breakChange*60);
+            setAlertText("break duration is over");
+            setTextTime("Break-Time");
+          }
+
+          else{
+            setWorkDuration(durationChange*60);
+            setAlertText("work duration is over");
+            setTextTime("Work-Time");
+
+          }
+
+          return;   
+        }
+        
+        setWorkDuration(workDuration - 1);
+      }, 1000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [flag,workDuration]);
+
+
+
+  function handleStart(){
+    setFlag(true);
+    setDisable(true);
+    setStopDisablity(false);
+    setResetDisablity(false);
+  }
+
+  function Stop(){
+    console.log(Timer(workDuration));
+    setFlag(false);
+    setStopDisablity(true);
+    setDisable(false);
+    setResetDisablity(false);
+  }
+
+
+  function handleReset(){
+    setWorkDuration(25*60);
+    setDisable(false);
+    setResetDisablity(true);
+  }
+
+return (
+  <div id="main">
+    {
+      Timer(workDuration)
+    }
+    
+    <div className='start-time'>{textTime}</div>
+    
+    <div>
+      <button disabled= {disabled} onClick={handleStart} data-testid='start-btn'>start</button>
+      <button disabled= {stopDisability} onClick={Stop} data-testid='stop-btn'>Stop</button>
+      <button disabled= {resetDisability} onClick={handleReset} data-testid='reset-btn'>Reset</button>
     </div>
+
+    <div>
+      <input onChange={(e) => setDurationChange(e.target.value)} type='number' required placeholder='work duration'></input>
+      <input onChange={(e)=> setBreakChange(e.target.value)} type='number' placeholder='break duration' required></input>
+      <button onClick={handleCLick}>set</button>
+    </div>
+  </div>
   )
 }
 
